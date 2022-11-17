4136111da7017e1b4c3eed4f864cc09fdc51af80
ASUS <tuntph21348@fpt.edu.vn>
2022-11-17 07:36:06 +0700

    tu_2

--- app/src/main/java/cp17304_n3/fpoly/test4/MainActivity.java
+++ app/src/main/java/cp17304_n3/fpoly/test4/MainActivity.java
@@ -3,13 +3,14 @@ package cp17304_n3.fpoly.test4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        btnA = findViewById(R.id.btnA);
     }
 }
